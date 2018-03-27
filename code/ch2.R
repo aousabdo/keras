4421151391161a0c71718c49cd721bb4e91ba2e6
Aous Abdo <aousabdo@gmail.com>
2018-03-26 22:38:16 -0700

    Added tensor slicing code

--- code/ch2.R
+++ code/ch2.R
@@ -103,7 +103,53 @@ print(metrics)
 network %>% predict_classes(test_images[1:10, ])
 
 # tensros
+# 1D tensors: Tensors of rank 1
 x <- 1:5
 print(str(x))
 print(str(as.array(x)))
 print(dim(as.array(x)))
+
+# 2D tensors: tensors of rank 2
+x <- matrix(1:30, nrow = 5, ncol = 6)
+print(x)
+dim(x)
+
+# 3D tensors: tensors of rank 3
+x <- array(data = 1:90, dim = c(5, 6, 3))
+print(x)
+print(dim(x))
+
+
+train_images <- mnist$train$x
+train_labels <- mnist$train$y
+test_images <- mnist$test$x
+test_labels <- mnist$test$y
+
+print(str(train_images))
+print(length(train_images))
+
+# train_images is of shape
+print(dim((train_images)))
+
+# train_images is a tensor of length
+print(length(dim(train_images)))
+
+print(typeof(train_images))
+
+print(length(train_images[1, , ]))
+print(train_images[1, , ])
+View(train_images[3, , ])
+plot(as.raster(train_images[623, , ], max = max(train_images[623, , ])))
+plot(as.raster(train_images[60000, , ], max = 255))
+
+dim(train_labels)
+train_labels[60000]
+#===========================================================#
+# tensor operations
+my_slice <- train_images[10:99, , ]
+print(dim(my_slice))
+
+plot(as.raster(my_slice[4, , ], max = 255))
+plot(as.raster(my_slice[4,15:28 , ], max = 255))
+plot(as.raster(my_slice[4,15:28 , 15:28], max = 255))
+
